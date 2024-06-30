388d526b3c955112cdf9509403a087b4d29807dc
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-29 19:37:10 -0700

    pushed a structure to implement priority queue

---
+++ min-Heap-Skeleton.py
@@ -0,0 +1,37 @@
+class MinHeap:
+    def __init__(self):
+        self.arr = []
+    
+    #here are the main heap functions
+    def insert(self,ele):
+        pass
+
+    def extractMin(self):
+        #return the minimum value from heap
+        pass
+    
+    def decreaseKey(self,key):
+        #reduces the value to some provided value does not delete it
+        pass
+
+    def delete(self,ele):
+        #deletes a element from the Heap
+        pass
+    
+    
+    #given below are Utility functions
+    def parent(self,i):
+        #return the index of parent provided ith element
+        pass
+    
+    def lChild(self,i):
+        #return index of Left Child of ith index
+        pass
+    
+    def rChild(self,i):
+        #return index of Right Child of ith index
+        pass
+
+    def minHeap(self):
+        #creates a min heap or maintains the min heap functionality
+        pass
