d537e4a8aa4220f6648ffd5f9743aa87ad3f45d0
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-07-04 15:53:49 -0700

    pushed decrease key code

--- min-Heap-Deletion.py
+++ min-Heap-Deletion.py
@@ -38,3 +38,21 @@ def extractMin(heap):
   heap.pop()
   heapify_down(heap, 0) #this will balance the min heap
   return minVal
+
+
+def parent(i):
+  return (i - 1) //2 # return parent index of Ith Node
+
+
+def decreaseKey(heap,i,x): # here I is the index and x is value to replace value at I, 
+  #decrease work on the premise of Insertion in Heap here we replace given value with value provided and then Heap Up to 
+  #satisfy the Min heap properties
+  heap[i] = x
+
+  while i > 0 and heap[parent(i)] > heap[i]:
+    p = parent(i)
+    heap[i],heap[p] = heap[p],heap[i]
+    i = p
+  
+   
+  
